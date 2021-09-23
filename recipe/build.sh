61953d648c96aba4d96d7684d15e7b217baa6194
Tobias Fischer <info@tobiasfischer.info>
2021-09-24 07:45:31 +1000

    Fix freeimage cross-compile error

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,7 +10,10 @@ cmake ${CMAKE_ARGS} .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib \
-      -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True
+      -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True \
+      -DFREEIMAGE_RUNS:BOOL=ON \
+      -DFREEIMAGE_RUNS__TRYRUN_OUTPUT:STRING="" \
+      -DFREEIMAGE_COMPILES:BOOL=ON
 
 cmake --build . --config Release
 cmake --build . --config Release --target install
