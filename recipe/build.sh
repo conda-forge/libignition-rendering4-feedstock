911aef163fb6f2fcf4d4acedb26445b6050c7e2c
Silvio Traversaro <silvio@traversaro.it>
2021-10-06 09:39:24 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,7 @@
 mkdir build
 cd build
 
+export VERBOSE=1
 export CFLAGS="${CFLAGS} -DGLX_GLXEXT_LEGACY"
 export CXXFLAGS="${CXXFLAGS} -DGLX_GLXEXT_LEGACY"
 
@@ -17,6 +18,7 @@ cmake ${CMAKE_ARGS} .. \
 
 cmake --build . --config Release
 cmake --build . --config Release --target install
+
 # UNIT_Heightmap_TEST disabled for https://github.com/conda-forge/libignition-rendering4-feedstock/issues/10
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
 ctest --output-on-failure -C Release -E "INTEGRATION|PERFORMANCE|REGRESSION|UNIT_RenderingIface_TEST|check_UNIT_RenderingIface_TEST|UNIT_Heightmap_TEST"
