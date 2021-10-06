7123db7612b642b809207ee85b961b776335ac50
Silvio Traversaro <silvio@traversaro.it>
2021-10-06 22:02:04 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -20,5 +20,5 @@ cmake --build . --config Release --target install
 
 # UNIT_Heightmap_TEST disabled for https://github.com/conda-forge/libignition-rendering4-feedstock/issues/10
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-ctest --output-on-failure -C Release -E "INTEGRATION|PERFORMANCE|REGRESSION|UNIT_RenderingIface_TEST|check_UNIT_RenderingIface_TEST|UNIT_Heightmap_TEST"
+ctest --extra-verbose --output-on-failure -C Release -E "INTEGRATION|PERFORMANCE|REGRESSION|UNIT_RenderingIface_TEST|check_UNIT_RenderingIface_TEST|UNIT_Heightmap_TEST"
 fi
